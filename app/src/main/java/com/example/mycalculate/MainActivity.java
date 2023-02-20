adbc49df5a81fbde0bd503e8b338dcbe90bd9bc4
sadfattycat <sadfattycat@mail.ru>
2023-02-20 11:41:03 +0300

    Mini calculator with the option addition

---
+++ app/src/main/java/com/example/mycalculate/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.mycalculate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private EditText number_field_1;
+    private EditText number_field_2;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = findViewById(R.id.resultTextView);
+        number_field_1 = findViewById(R.id.number_field_1);
+        number_field_2 = findViewById(R.id.number_field_2);
+        Button add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float num1 = Float.parseFloat(number_field_1.getText().toString());
+                float num2 = Float.parseFloat(number_field_2.getText().toString());
+                float res = num1 + num2;
+                resultTextView.setText(String.valueOf(res));
+            }
+        });
+
+    }
+}
